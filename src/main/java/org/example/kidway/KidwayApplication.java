c6930a00ce179da2aaae4a05863f265660d38669
pavel <pasha.hogay@gmail.com>
2025-03-18 19:10:46 +0100

    V1

---
+++ src/main/java/org/example/kidway/KidwayApplication.java
@@ -0,0 +1,13 @@
+package org.example.kidway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kidway/KidwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kidway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
